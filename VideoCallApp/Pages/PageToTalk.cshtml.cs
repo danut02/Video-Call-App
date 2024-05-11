e39259ea5cde8def1de6aac4dd54346ebdc70388
Tavy780 <octavstefota2003@gmail.com>
2024-05-11 16:27:20 +0300

    little changes

--- VideoCallApp/Pages/PageToTalk.cshtml.cs
+++ VideoCallApp/Pages/PageToTalk.cshtml.cs
@@ -1,18 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using VideoCallApp.Models;
+using VideoCallApp.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace VideoCallApp.Pages
 {
     public class PageToTalkModel : PageModel
     {
+        private readonly VideoCallApplicationDbContext theDatabase;
+        public PageToTalkModel(VideoCallApplicationDbContext theContext)
+        {
+            theDatabase = theContext;
+        }
         [BindProperty]
         public int UsersId { get; set; }
         [BindProperty]
         public int TheOtherUserId { get; set; }
+        [BindProperty]
+        public User theUser { get; set; }
         public void OnGet(int otherUserId, int userId)
         {
             TheOtherUserId=otherUserId;
-            UsersId=userId; 
+            UsersId=userId;
+            theUser = theDatabase.Users.Find(userId);
         }
     }
 }
